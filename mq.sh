a3e80cb8124b9492de7a252ea92819f40f4358c0
Shellz <isdrupter@users.noreply.github.com>
2016-04-03 13:53:38 -0500

    Update mq.sh
    Fix daemonize issue

--- mq.sh
+++ mq.sh
@@ -1,37 +1,26 @@
-#!/bin/sh
-################################################################
-# MQTT Shell (Using pipes)
-
-# export PATH=/usr/sbin:/bin:/usr/bin:/sbin:/var/bin
+# Mqtt Exec Handler - ShellzRus 2016
+######################################################
+# Usage: nohup ash mq &
+#export PATH=/usr/sbin:/bin:/usr/bin:/sbin:/custom
+run(){
 host=localhost
 pipe=p
-binStream=/tmp/tmp_binary
 binDest=/tmp/bin
-
-#pidfile=/var/run/subclient
-# remove pipe on exit to keep things sane
-trap "rm -f $pipe;rm enc;rm denc;rm output" EXIT INT
-# Creat our pipe
 if [ ! -p p ]; then
     mkfifo $pipe
 fi
-# Daemonize subclient and write to pipe (no hup needed!)
-(subclient -h $host -t shell > $pipe) &
-#pid=$!;echo pid="$!" > $pidfile &
-# This is the magic. From mqsh, typing "quit' cleans everything up too.
-
+(subclient -h $host -t shell > $pipe) & # Daemonize
 while read line <$pipe
 do
   echo $line > enc
-
     base64 -d enc > denc;> enc
-    if grep -q "_quit_" denc; then
+    if grep -q "_quit_" denc; then # Quit if we receive _quit_
+        rm $pipe enc denc out
         break
     fi
-# If sending a binary, insert _binary_ as the first line, check for that,
+# If the first line is _binary_, do not execute it,
     if grep -q "_binary_" denc; then
-      #echo "$line" > $binStream
-# remove it and save the binary to /tmp/bin
+# but sed the line out an save it, than report exit status
       sed '1d' denc > $binDest
       if ([ $? -eq "0" ]);then 
         echo 'Successfully transfered binary :' > output 
@@ -41,10 +30,12 @@ do
       fi
 # publish status
       pubclient -h $host -t data -f output;>output
-    else
+    else # otherwise execute whatever
       sh denc > output
       pubclient -h $host -t data -f output;>output
     fi
 done
-#pid=`cat pidfile`;(kill $pid  && rm $pidfile) &>2 > /dev/null
-exit
+}
+
+trap "" 1 2 8 #trap signals
+(run) & 2>/dev/null # to detach, i think we need stder redirected
